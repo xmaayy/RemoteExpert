6c190f4f2291780c2f75572b3c26b1e8682b6da0
Mattias Lightstone <matt1light@gmail.com>
2020-12-27 19:14:05 -0500

    Reformatting to be less reliant on global variables

--- socket_webrtc/src/dssclient.ts
+++ socket_webrtc/src/dssclient.ts
@@ -17,26 +17,35 @@ const SDP_TYPE = {
     CANDIDATE: 3,
 }
 
-const iceSeparatorCharacter = "|"
+// Ice candidate messages consist of three fields delimited by a separation character
+// the default choice of character is |
+const ICE_SEPARATOR_CHAR = "|"
 
 // The client polls the signalling server to receive new offers, answers, and candidates left for its id
 // pollInterval determines how frequently the server should be polled
-const pollInterval = 100;
+const pollInterval = 2000;
 
+// References for the connection create div and the video streams div
 const divOffers = (<HTMLDivElement>window.document.getElementById("connect"));
 const divConsultingRoom = (<HTMLDivElement>window.document.getElementById("consultingRoom"));
+
+// References for each of the inputs 
 const inputMyId = (<HTMLInputElement>window.document.getElementById("myId"));
 const inputPeerId = (<HTMLInputElement>window.document.getElementById("peerId"));
 const inputUri = (<HTMLInputElement>window.document.getElementById("signallingServerUrl"));
+const inputTwoWayVideo =(<HTMLInputElement>window.document.getElementById("twoWayVideo"));
+
+// References for the two buttons to control activity
+// Poll offers should poll the signalling server for 
 const btnPollOffers = (<HTMLButtonElement>window.document.getElementById("pollOffers"));
 const btnCreateOffer = (<HTMLButtonElement>window.document.getElementById("createOffer"));
+
 const localVideo = (<HTMLMediaElement>window.document.getElementById("localVideo"));
 const remoteVideo = (<HTMLMediaElement>window.document.getElementById("remoteVideo"));
+// TODO remove these two variables below
+var exitPoll = false;
 
 // variables
-var localStream;
-var remoteStream;
-var rtcPeerConnection;
 // needed some defaults
 const iceServers: RTCConfiguration = {
     iceServers: [
@@ -56,15 +65,13 @@ btnCreateOffer.onclick = function () {
     } else {
         // Create an offer
         // We need to start showing our stream on the 'local' client
-        navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
-            localStream = stream;
-            localVideo.srcObject = stream;
-            offer();
-        }).catch(function (err) {
-            console.log('an error ocurred when accessing media devices', err);
-        });
-        divOffers.style.display = "none";
-        divConsultingRoom.style.display = "block";
+        initializeLocalVideoStream(iceServers).then((rtcPeerConnection: RTCPeerConnection) => {
+            offer(rtcPeerConnection).then(() => {
+                divOffers.style.display = "none";
+                divConsultingRoom.style.display = "block";
+                poll(rtcPeerConnection, inputUri.value, inputMyId.value, exitPoll);
+            })
+        })
     }
 };
 
@@ -74,93 +81,115 @@ btnPollOffers.onclick = () => {
     } else if (inputMyId.value === '') {
         alert("please input your id")
     } else {
-        poll();
-        // navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
-        //     localStream = stream;
-        //     localVideo.srcObject = stream;
-        //     isCaller = true;
-        //     poll();
-        // }).catch(function (err) {
-        //     console.log('an error ocurred when accessing media devices', err);
-        // });
+        if(inputTwoWayVideo.value){
+            initializeLocalVideoStream(iceServers).then((rtcPeerConnection: RTCPeerConnection) => {
+                divOffers.style.display = "none";
+                divConsultingRoom.style.display = "block";
+                poll(rtcPeerConnection, inputUri.value, inputMyId.value, exitPoll);
+            })
+        } else {
+            const rtcPeerConnection = initializeRtc(iceServers);
+            divOffers.style.display = "none";
+            divConsultingRoom.style.display = "block";
+            poll(rtcPeerConnection, inputUri.value, inputMyId.value, exitPoll);
+        }
         // We need to start showing our stream on the 'local' client
-        divOffers.style.display = "none";
-        divConsultingRoom.style.display = "block";
     }
 }
 
-const offer = function() {
-    // on ready received, this should only be acted on by the creator of the room
-    // (known here as the caller) because they're the ones that are going to be
-    // broadcasting to the rest of the clients in the room
-    rtcPeerConnection = new RTCPeerConnection(iceServers);
+const initializeRtc = (iceServers: RTCConfiguration): RTCPeerConnection => {
+    const rtcPeerConnection = new RTCPeerConnection(iceServers);
     rtcPeerConnection.onicecandidate = onIceCandidate;
     rtcPeerConnection.ontrack = onAddStream;
-    rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
-    rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
-    rtcPeerConnection.createOffer()
-        .then(sessionDescription => {
-            // once the tracks have been added and we create the sdp params
-            // from the broadcaster side we can send the offer to the stuni
-            // server (local) for rebroadcasting to the room
-            rtcPeerConnection.setLocalDescription(sessionDescription);
-            axios.post(inputUri.value+'/data/'+ inputPeerId.value, {
-                MessageType: SDP_TYPE.OFFER,
-                Data: sessionDescription.sdp
-            }).then((res) => {
-                poll();
-            }).catch((err) => {
-                console.error(err);
-                console.log('Could not send offer')
-            })
-        })
-        .catch(error => {
-            console.log(error)
-        })
+    return rtcPeerConnection
 }
 
+const initializeLocalVideoStream = (iceServers: RTCConfiguration): Promise<RTCPeerConnection> => {
+    return new Promise((resolve, reject) => {
+        navigator.mediaDevices.getUserMedia(streamConstraints).then(function (stream) {
+            const localStream = stream;
+            localVideo.srcObject = stream;
 
-const handleOffer = function(sdp) {
-    // this ready offer should only be acted on by the one joining a room because
-    // they're going to be listening to the caller. so they dont add any media
-    // tracks to their session description.
-    console.log('Handling offer')
-    rtcPeerConnection = new RTCPeerConnection(iceServers);
-    rtcPeerConnection.onicecandidate = onIceCandidate;
-    rtcPeerConnection.ontrack = onAddStream;
-    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription({sdp: sdp, type: 'offer'})).then(() => {
-        rtcPeerConnection.createAnswer()
-            .then(sessionDescription => {
-                // once we've created a session description without any media tracks
-                // send the answer back through the socket
-                rtcPeerConnection.setLocalDescription(sessionDescription);
-                axios.post(inputUri.value+'/data/'+inputPeerId.value, {
-                    MessageType: SDP_TYPE.ANSWER,
-                    Data: sessionDescription.sdp,
-                })
-            })
-            .catch(error => {
-                console.error(error)
-            })
-    }).catch((err) => {
-        console.log('Could not set remote description')
-        console.error(err);
+            const rtcPeerConnection = initializeRtc(iceServers);
+            rtcPeerConnection.addTrack(localStream.getTracks()[0], localStream);
+            rtcPeerConnection.addTrack(localStream.getTracks()[1], localStream);
+
+            divOffers.style.display = "none";
+            resolve(rtcPeerConnection);
+        }).catch(function (err) {
+            console.log('an error ocurred when accessing media devices', err);
+        });
     })
 }
 
+const offer = function(rtcPeerConnection: RTCPeerConnection): Promise<void> {
+    // on ready received, this should only be acted on by the creator of the room
+    // (known here as the caller) because they're the ones that are going to be
+    // broadcasting to the rest of the clients in the room
+    return new Promise((resolve, reject) => {
+        rtcPeerConnection.createOffer()
+            .then(sessionDescription => {
+                // once the tracks have been added and we create the sdp params
+                // from the broadcaster side we can send the offer to the stuni
+                // server (local) for rebroadcasting to the room
+                rtcPeerConnection.setLocalDescription(sessionDescription);
+                axios.post(inputUri.value+'/data/'+ inputPeerId.value, {
+                    MessageType: SDP_TYPE.OFFER,
+                    Data: sessionDescription.sdp
+                }).then((res) => {
+                    resolve();
+                }).catch((err) => {
+                    console.log('Could not send offer')
+                    reject(err);
+                })
+            })
+            .catch(err => {
+                reject(err);
+            })
+        })
+}
+
+
+const handleOffer = function(sdp: string, rtcPeerConnection: RTCPeerConnection, uri: string, peerId: string): Promise<void> {
+    // this ready offer should only be acted on by the one joining a room because
+    // they're going to be listening to the caller. so they dont add any media
+    // tracks to their session description.
+    return new Promise((resolve, reject) => {
+        console.log('Handling offer')
+        rtcPeerConnection.setRemoteDescription(new RTCSessionDescription({sdp: sdp, type: 'offer'})).then(() => {
+            rtcPeerConnection.createAnswer()
+                .then(sessionDescription => {
+                    // once we've created a session description without any media tracks
+                    // send the answer back through the socket
+                    rtcPeerConnection.setLocalDescription(sessionDescription);
+                    axios.post(uri+'/data/'+peerId, {
+                        MessageType: SDP_TYPE.ANSWER,
+                        Data: sessionDescription.sdp,
+                    }).then((response) => {
+                        resolve()
+                    }).catch(err => {reject(err)})
+                .catch(error => {
+                    reject(error)
+                })
+        }).catch((err) => {
+            reject(err);
+        })
+    })})
+}
+
 // This is the main event loop polling the signalling server for incoming connections
-const poll = () => {
+const poll = (rtcPeerConnection: RTCPeerConnection, uri: string, myId: string, exitPoll: boolean) => {
     setTimeout(() => {
         axios.get(inputUri.value+'/data/'+inputMyId.value).then((res) => {
                 switch(res.data.MessageType){
                     case SDP_TYPE.OFFER:
-                        handleOffer(res.data.Data)
+                        handleOffer(res.data.Data, rtcPeerConnection, inputUri.value, inputPeerId.value)
                         break
                     case SDP_TYPE.ANSWER:
-                        handleAnswer(res.data.Data)
+                        handleAnswer(res.data.Data, rtcPeerConnection)
                         break
                     case SDP_TYPE.CANDIDATE:
-                        handleCandidate(res.data.Data, res.data.IceDataSeparator)
+                        handleCandidate(res.data.Data, rtcPeerConnection, res.data.IceDataSeparator)
                         break
                     default: 
                         break
@@ -173,28 +202,25 @@ const poll = () => {
                 console.error(err);
             }
         })
-        poll();
+        if (!exitPoll){
+            poll(rtcPeerConnection, uri, myId, exitPoll);
+        }
     }, pollInterval)
 }
 
-var handleAnswer = (sdp) => {
-    rtcPeerConnection.setRemoteDescription(new RTCSessionDescription({sdp: sdp, type: 'answer'})).then((remoteDescription) => {
-        console.log('Successfully processed answer');
-    }).catch((err) => {
-        console.log('Could not handle answer');
-        console.error(err);
-    });
+var handleAnswer = (sdp: string, rtcPeerConnection: RTCPeerConnection): Promise<void> => {
+    return rtcPeerConnection.setRemoteDescription(new RTCSessionDescription({sdp: sdp, type: 'answer'}))
 }
 
-var handleCandidate = (data, separatorChar) => {
+var handleCandidate = (data: string, rtcPeerConnection: RTCPeerConnection, separatorChar: string) => {
     console.log('Handling Ice Candidate')
     const ice = data.split(separatorChar);
     var candidate = new RTCIceCandidate({
         sdpMid: ice[2],
-        sdpMLineIndex: ice[1],
+        sdpMLineIndex: parseInt(ice[1]),
         candidate: ice[0]
     });
-    rtcPeerConnection.addIceCandidate(candidate);
+    return rtcPeerConnection.addIceCandidate(candidate);
 }
 
 // handler functions
@@ -206,15 +232,13 @@ function onIceCandidate(event) {
             Data: 
                 [event.candidate.candidate,
                 event.candidate.sdpMLineIndex,
-                event.candidate.sdpMid].join(iceSeparatorCharacter),
-            IceDataSeparator: "|"
+                event.candidate.sdpMid].join(ICE_SEPARATOR_CHAR),
+            IceDataSeparator: ICE_SEPARATOR_CHAR
         })
     }
 }
 
 function onAddStream(event) {
     console.log('Adding stream')
-    debugger;
     remoteVideo.srcObject = event.streams[0];
-    remoteStream = event.stream;
 }
