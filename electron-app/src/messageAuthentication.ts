bee9ac47c1ab0bab8754cebf8f89d146b03c143e
matt1light <matt1light@gmail.com>
2021-01-10 20:27:16 -0500

    HMAC-SHA256 is more secure than HMAC-SHA1 with negligable performance cost

--- electron-app/src/messageAuthentication.ts
+++ electron-app/src/messageAuthentication.ts
@@ -2,12 +2,12 @@ import CryptoJS from 'crypto-js';
 
 export const sign = (message: Object, key: string): string => {
     const stringMessage = JSON.stringify(message);
-    const signature = CryptoJS.HmacSHA1(stringMessage, key).toString();
+    const signature = CryptoJS.HmacSHA256(stringMessage, key).toString();
     return signature;
 }
 
 export const verify = (message: Object, signature: string, key: string): Boolean => {
     const stringMessage = JSON.stringify(message);
-    const calculatedSignature = CryptoJS.HmacSHA1(stringMessage, key).toString()
+    const calculatedSignature = CryptoJS.HmacSHA256(stringMessage, key).toString()
     return calculatedSignature === signature;
 }
